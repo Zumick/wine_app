547ea1f65288f79ef3843a5b129ceac49c983546
Zumick <89249054+Zumick@users.noreply.github.com>
2026-03-27 09:35:58 +0100

    Add files via upload

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
+import os
 import sqlite3
 
-DB_NAME = "wine.db"
+DB_DIR = os.environ.get("DB_DIR", "/data")
+DB_NAME = os.path.join(DB_DIR, "wine.db")
 
 def get_connection():
     conn = sqlite3.connect(DB_NAME)
